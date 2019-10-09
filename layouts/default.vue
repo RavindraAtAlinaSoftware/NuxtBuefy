134414df8b2ea39e0bcac53cbec2e6e61126bde1
Chinmay <o_chinmay.k@fusionsol.com>
2019-10-09 20:17:38 +0530

    Table Added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,6 @@
 <template>
   <div>
-    <nav
-      class="navbar header has-shadow is-primary"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div class="navbar-brand">
-        <a class="navbar-item" href="/">
-          <img src="~assets/buefy.png" alt="Buefy" height="28" />
-        </a>
-
-        <div class="navbar-burger">
-          <span />
-          <span />
-          <span />
-        </div>
-      </div>
-    </nav>
+    <my-nav></my-nav>
 
     <section class="main-content columns">
       <aside class="column is-2 section">
@@ -40,7 +24,13 @@
 </template>
 
 <script>
+import MyNav from '~/layouts/MyNav'
+
 export default {
+  components: {
+    'my-nav': MyNav
+  },
+
   data() {
     return {
       items: [
@@ -53,6 +43,11 @@ export default {
           title: 'Inspire',
           icon: 'lightbulb',
           to: { name: 'inspire' }
+        },
+        {
+          title: 'Table',
+          icon: 'table',
+          to: { name: 'table' }
         }
       ]
     }
---
+++ pages/table.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1 class="title">Users</h1>
+    <b-table :data="users" :columns="columns"></b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Table',
+
+  data() {
+    return {
+      users: [],
+      columns: [
+        {
+          field: 'id',
+          label: 'ID',
+          width: '40',
+          numeric: true
+        },
+        {
+          field: 'name',
+          label: 'Name'
+        },
+        {
+          field: 'phone',
+          label: 'Phone',
+          centered: true
+        },
+        {
+          field: 'email',
+          label: 'Email'
+        }
+      ]
+    }
+  },
+  async mounted() {
+    this.users = await this.$axios.$get(
+      'https://jsonplaceholder.typicode.com/users'
+    )
+  }
+}
+</script>
